1980f9affcbef419d776a7cc6ffde8a87bd93a82
suridsr7874 <50363674+suridsr7874@users.noreply.github.com>
2019-12-29 11:46:17 +0530

    Add files via upload

---
+++ ng-switch/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ng-switch';
+  t1:string="d";
+
+arr=['hyd', 'bnglr', 'chn'];
+t1: string;
+fun() {
+  this.arr.push(this.t1)
+}
+  add() {
+    alert .arr1.push(this.add);
+    alert(this.add);
+  }
+}
---
+++ ngif/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ngif';
+  arr:number[]=[10,20,30];
+ 
+  arr1: string[]=["hyd","banglr","chennai","mumbai"];
+
+}
